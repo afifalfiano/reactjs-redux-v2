b120dc03c6b2a5eee6ed4a150599c9f8ecf0f47f
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-03 09:40:27 +0700

    fix: fix state

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id, description } = props.item;
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }));
+  }
+
+  const removeToCartHandler = () => {
+    dispatch(cartActions.removeItemCart(id));
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +33,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeToCartHandler}>-</button>
+          <button onClick={addToCartHandler}>+</button>
         </div>
       </div>
     </li>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS  = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'First Book',
+    description:  'The first book'
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'Pen',
+    description:  'The first pen'
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map(item => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+        title={item.title}
+        price={item.price}
+        description={item.description}
+        key={item.id}
+        id={item.id}
         />
+        ))}
       </ul>
     </section>
   );
